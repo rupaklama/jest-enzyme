cdfd51a7dcdbbe3b2f746123484b06878067c502
pak <rupaklamadev@gmail.com>
2021-10-27 22:59:17 -0400

    added data-test to select an element

--- demo_jest_enzyme/src/App.js
+++ demo_jest_enzyme/src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Learn React!</h1>
+    <div className='App' data-test='component-app'>
+      <h1 data-test='counter-display'>The counter is currently</h1>
+
+      <button data-test='increment-button'>Increment counter</button>
     </div>
   );
 }
--- demo_jest_enzyme/src/App.test.js
+++ demo_jest_enzyme/src/App.test.js
@@ -1,4 +1,4 @@
-// Enzyme creates a virtual dom & it's rendering methods for Unit/Integration Testing
+// Enzyme creates a virtual dom & provides rendering methods for Unit/Integration Testing
 import Enzyme, { shallow } from 'enzyme';
 
 // to help translate react code into Enzyme code
@@ -9,10 +9,22 @@ import App from './App';
 // configure enzyme
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test('renders non-empty component without crashing', () => {
+// to keep code DRY
+// custom factory function to create a ShallowWrapper for the App component
+const setup = () => shallow(<App />);
+
+test('renders non-empty component without error', () => {
   // note - Shallow render only renders Parent component but not Child components
   // Shallow takes JSX & returns shallow wrapper component - output of shallow rendering
-  const wrapper = shallow(<App />);
+  // const wrapper = shallow(<App />);
+  const wrapper = setup();
+
+  //.find(selector) => ShallowWrapper
+  // find() - to find every node in the render tree of the current wrapper that matches the provided Selector.
+  // Using attribute syntax ([attribute="value"] as a Test Selector.
+  // note - Matches elements with an attr attribute whose value is exactly value — the string inside the quotes.
+  const appComponent = wrapper.find("[data-test='component-app']"); // attrib name/value
+  expect(appComponent.length).toBe(1);
 
   // assertion with expect(subject) - global function
   // expect statement gives us access to a number of "matchers" that let us validate different things
@@ -21,8 +33,29 @@ test('renders non-empty component without crashing', () => {
   // Returns whether or not any nodes exist in the wrapper.
   // Or, if a selector is passed in, whether that selector has any matches in the wrapper
 
-  // one of the many Shallow rendering methods.
+  // .exists() is one of many Shallow rendering methods.
+  // https://enzymejs.github.io/enzyme/docs/api/ShallowWrapper/exists.html
+
+  // console.log(wrapper.debug());
   // Returns an HTML-like string of the wrapper for debugging purposes.
   // Useful to print out to the console when tests are not passing when you expect them to
-  // console.log(wrapper.debug());
 });
+
+test('renders button', () => {
+  const wrapper = setup();
+  const button = wrapper.find("[data-test='increment-button']");
+
+  // saying that there should be only One Element with that particular class name
+  expect(button.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = wrapper.find("[data-test='counter-display']");
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter display starts at 0', () => {});
+
+// here we test functionality
+test('clicking button increments counter', () => {});
